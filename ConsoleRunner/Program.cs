ec528ddcdb219e3b0dceea4834a721897966aae9
seymen <oseymen@gmail.com>
2012-06-05 18:28:54 +0100

    Improved runner with LogEventInfo properties

--- ConsoleRunner/Program.cs
+++ ConsoleRunner/Program.cs
@@ -15,28 +15,43 @@ namespace Gelf4NLog.ConsoleRunner
         {
             while (true)
             {
-                var nextComic = GetNextComic();
+                var comic = GetNextComic();
 
                 var eventInfo = new LogEventInfo
                                     {
-                                        Message = nextComic,
+                                        Message = comic.Title,
                                         Level = LogLevel.Info,
                                     };
+                eventInfo.Properties.Add("Publisher", comic.Publisher);
+                eventInfo.Properties.Add("ReleaseDate", comic.ReleaseDate);
+
                 Logger.Log(eventInfo);
 
                 Thread.Sleep(1000);
             }
         }
 
-        private static string GetNextComic()
+        private static Comic GetNextComic()
         {
             var nextComicIndex = Random.Next(1, 400);
 
             using (var csv = new CsvReader(new StreamReader("comics.csv"), false))
             {
                 csv.MoveTo(nextComicIndex);
-                return string.Format("{0} from {1} was released on {2}", csv[2], csv[1], csv[0]);
+                return new Comic
+                {
+                    Title = csv[2],
+                    Publisher = csv[1],
+                    ReleaseDate = csv[0]
+                };
             }
         }
+    }
+
+    class Comic
+    {
+        public string Title { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Publisher { get; set; }
     }
 }
